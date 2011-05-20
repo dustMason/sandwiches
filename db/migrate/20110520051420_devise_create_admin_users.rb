fcad989cf1dc01cad3de5720fba51be1b7af07d0
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2011-05-19 23:43:20 -0700

    revert migration

--- db/migrate/20110520051420_devise_create_admin_users.rb
+++ db/migrate/20110520051420_devise_create_admin_users.rb
@@ -9,15 +9,14 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
-      t.string :login
 
       t.timestamps
     end
 
     # Create a default user
-    AdminUser.create!(:login => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
 
-    add_index :admin_users, :login,                :unique => true
+    add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
     # add_index :admin_users, :confirmation_token,   :unique => true
     # add_index :admin_users, :unlock_token,         :unique => true
