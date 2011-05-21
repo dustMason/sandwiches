5fb4b6377add0efa29c72c4c4b07d0c24eb58fca
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2011-05-20 23:32:44 -0700

    default should be boolean instead of int

--- db/migrate/20110521020832_add_wantsemail_to_users.rb
+++ db/migrate/20110521020832_add_wantsemail_to_users.rb
@@ -1,6 +1,6 @@
 class AddWantsemailToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :wants_email, :boolean, :default => 1
+    add_column :users, :wants_email, :boolean, :default => true
     User.update_all :wants_email => true
   end
 
